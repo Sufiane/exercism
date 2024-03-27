849d94d324f6c8abd7b776fde7398d2dab664cb8
Sufiane <su.souissi@gmail.com>
2024-03-27 17:05:26 +0100

    feat: second solution for run-length-encoding

--- typescript/run-length-encoding/run-length-encoding.ts
+++ typescript/run-length-encoding/run-length-encoding.ts
@@ -21,51 +21,23 @@ export function encode(encodedValue: string): string {
 
 }
 
-const isNumber = (value: string) => /[0-9]/.test(value)
-
-
 export function decode(valueToDecode: string): string {
-    return valueToDecode.split('')
-        .reduce<{ repeat: number, value: string }[]>((acc, value) => {
-            const lastEntry = acc.at(-1)
+    if (valueToDecode === '') {
+        return ''
+    }
 
-            if (lastEntry) {
-                if (isNumber(value)) {
-                    if (lastEntry.value !== '') {
-                        acc.push({
-                            repeat: parseInt(value),
-                            value: '',
-                        })
-                    } else {
-                        lastEntry.repeat = lastEntry.repeat * 10 + parseInt(value)
-                    }
-                } else {
-                    if (lastEntry.value === '') {
-                        lastEntry.value = value
-                    } else {
-                        acc.push({
-                            repeat: 1,
-                            value,
-                        })
-                    }
-                }
-            } else {
-                if (isNumber(value)) {
-                    acc.push({
-                        repeat: parseInt(value),
-                        value: '',
-                    })
-                } else {
-                    acc.push({
-                        repeat: 1,
-                        value,
-                    })
-                }
-            }
+    const result = valueToDecode.match(/\d*[A-Za-z\s]/g)
 
-            return acc
-        }, [])
-        .reduce((acc, entry) => {
-            return acc + entry.value.repeat(entry.repeat)
-        }, '')
+    if (!result) {
+        throw new Error('value to decode do not match format.')
+    }
+
+    return result.reduce((acc, match) => {
+        const [_, number, letter] = match.match(/(\d*)([A-Za-z\s]+)/)!
+
+        acc += `${letter.repeat(parseInt(number || '1'))}`
+
+        return acc
+    }, '')
 }
+
